c92e1f662e7b04948181be3a5e66eee920258a14
Bhaskar_here <145371100+bhaskarchatterjee970@users.noreply.github.com>
2024-05-09 00:14:58 +0530

    Update Projects.jsx

--- PORTFOLIO/src/Components/Projects/Projects.jsx
+++ PORTFOLIO/src/Components/Projects/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
               <h2 className='text-center font-semibold text-xl md:text-3xl'>Portfolio</h2>
               <img className='h-4/6 w-3/5 md:w-[260px] ml-14 md:mt-4' src='https://github.com/krrish9368/reactjs-portfolio-part-1/blob/main/portfolio.png?raw=true' />
               <div className='mt-6 w-full h-5 md:ml-4 flex items-center justify-center gap-x-6 font-bold text-[16px] md:text-xl '>
-                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Github</a></button>
+                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='https://github.com/bhaskarchatterjee970/CBTCIP/tree/main/PORTFOLIO'>Github</a></button>
                 <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Live Link</a></button>
               </div>
             </div>
@@ -21,7 +21,7 @@ function Projects() {
               <h2 className='text-center font-semibold text-[20px]  md:text-[28px]'>Customizable Password Generator </h2>
               <img className='h-full md:h-4/6 w-3/5 md:w-[260px] ml-14 md:ml-1 md:mt-1 rounded-xl' src="https://blog.nerdjfpb.com/wp-content/uploads/2022/02/1.-React-Password-Gen-Final-Output-1024x502.png" alt="" />
               <div className='mt-6 w-full h-5 md:ml-4 flex items-center justify-center gap-x-6 font-bold text-[16px] md:text-xl '>
-                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Github</a></button>
+                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='https://github.com/bhaskarchatterjee970/Password_Generator'>Github</a></button>
                 <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Live Link</a></button>
               </div>
             </div>
@@ -30,7 +30,7 @@ function Projects() {
               <h2 className='text-center font-semibold text-xl md:text-3xl'>Bubble Popper Game</h2>
               <img className='h-4/6 w-3/5 md:w-[260px] ml-10 md:ml-6 md:mt-4 rounded-xl'  src="https://wellgames.com/storage/uploads/screenshots/jpg/bubble-wobble_1611648405_0.jpg" alt="" />
               <div className='mt-6 w-full h-5 md:ml-4 flex items-center justify-center gap-x-6 font-bold text-[16px] md:text-xl '>
-                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Github</a></button>
+                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='https://github.com/bhaskarchatterjee970/BubbleGame'>Github</a></button>
                 <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Live Link</a></button>
               </div>
             </div>
@@ -39,7 +39,7 @@ function Projects() {
               <h2 className='text-center font-semibold text-xl md:text-3xl'>Mind Master</h2>
               <img className='h-4/6 w-3/5 md:w-[260px] ml-10 md:mt-4 rounded-xl' src="https://easyshiksha.com/online_courses/assets/games/number-guessing/images/banner.png" alt="" />
               <div className='mt-6 w-full h-5 md:ml-4 flex items-center justify-center gap-x-6 font-bold text-[16px] md:text-xl '>
-                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Github</a></button>
+                <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='https://github.com/bhaskarchatterjee970/NumberGuessing_Game'>Github</a></button>
                 <button className='text-gray-700 rounded-md bg-slate-200 px-4 py-2'><a href='#'>Live Link</a></button>
               </div>
             </div>
