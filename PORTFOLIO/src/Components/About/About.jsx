84b6679c48ada45fc98a542018ff698c25e31585
Bhaskar_here <145371100+bhaskarchatterjee970@users.noreply.github.com>
2024-11-13 20:45:19 +0530

    Update About.jsx

--- PORTFOLIO/src/Components/About/About.jsx
+++ PORTFOLIO/src/Components/About/About.jsx
@@ -11,16 +11,16 @@ function About() {
         <div className='text-white flex-col flex h-1/2 md:h-full w-full md:w-3/5 items-start justify-center px-2 md:px-5 '>
             <h1 className='text-[60px] md:text-[65px] ml-20 md:ml-0 font-bold md:font-semibold py-2 pt-8 md:pt-0
             hover:text-cyan-300 pl-4 '>About</h1>
-            <p className='text-xl md:text-2xl pl-4 font-semibold text-gray-300'> Hey, I'm Bhaskar Chatterjee, a Computer Science student at B.P. Poddar Institute of 
-              Management and Technology, Kolkata. With an 8.6 CGPA and an 86% score in schooling, I'm passionate about pushing boundaries.
-                <br/>
-                <br/>
-                💻 Specializing in Data Structures and Algorithms with Java, and adept at frontend development, I blend innovation with aesthetics to create 
-                  captivating digital experiences. Let's innovate and leave a mark on the digital landscape together!
+
+            <p className='text-xl md:text-2xl pl-4 font-semibold text-gray-300'>
+            👋 Hey, I'm Bhaskar Chatterjee, a Computer Science student at B.P. Poddar Institute of Management and Technology, Kolkata. With an 8.7 CGPA and an 86% score in schooling, I'm passionate about pushing boundaries.
+            <br/>
+            <br/>
+            💻 Specializing in Data Structures and Algorithms with Java, Oops, DBMS, SQL and adept at frontend development, I blend innovation with aesthetics to create captivating digital experiences. Let's innovate and leave a mark on the digital landscape together!
             </p>
-            <a href="https://docs.google.com/document/d/1rlD6_bcLTiYo4HbOK_kgKNQdfmpwBX5dcNrbMb9CDXs/edit?usp=sharing" download>
-              <button className='bg-white ml-3 text-black px-5 py-4 rounded-xl mt-4 mx-2 font-semibold hover:bg-slate-300'>
-                Download CV
+            <a href="https://github.com/bhaskarchatterjee970/Resume/blob/main/BhaskarResume.pdf" download>
+            <button class="bg-white ml-3 text-black px-5 py-4 rounded-xl mt-4 mx-2 font-semibold hover:bg-slate-300 transition-colors duration-300">
+            Download CV
               </button>
             </a>
         </div>
